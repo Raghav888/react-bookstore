f5e07e874ee6bfb6f3c4cadb13d38e1dddcdf260
Raghav Bang <raghav.bang@blueconchtech.com>
2022-03-14 11:56:24 +0530

    className added

--- src/components/shared/navbar/Navbar.js
+++ src/components/shared/navbar/Navbar.js
@@ -3,40 +3,40 @@ import "./Navbar.css"
 export function Navbar(){
     return(
         
-    <nav class="mantra-nav">
-    <div class="mantra-nav-title">
-        <a class="mantra-title" href="index.html"> BookStore </a>
+    <nav className="mantra-nav">
+    <div className="mantra-nav-title">
+        <a className="mantra-title" href="index.html"> BookStore </a>
     </div>
-    <div class="mantra-nav-search">
-        <input class="mantra-search-box mantra-textbox-classic mantra-highlight-box" placeholder="  Search" type="text " />
-        <button class="mantra-button mantra-primary-btn">
-      <i class="fa fa-search"></i> Search
+    <div className="mantra-nav-search">
+        <input className="mantra-search-box mantra-textbox-classic mantra-highlight-box" placeholder="  Search" type="text " />
+        <button className="mantra-button mantra-primary-btn">
+      <i className="fa fa-search"></i> Search
     </button>
     </div>
 
-    <div class="mantra-nav-footer">
+    <div className="mantra-nav-footer">
         <a href="./Html/login.html">
-            <button class="mantra-button mantra-primary-btn mantra-login-btn">
+            <button className="mantra-button mantra-primary-btn mantra-login-btn">
         Login
       </button>
         </a>
 
-        <div class="mantra-badge">
-            <a class="mantra-menu-icon" href="./Html/wishlist.html">
-                <span class="mantra-icon">
-          <i class="fa fa-heart"></i>
-          <span class="mantra-count">5</span>
+        <div className="mantra-badge">
+            <a className="mantra-menu-icon" href="./Html/wishlist.html">
+                <span className="mantra-icon">
+          <i className="fa fa-heart"></i>
+          <span className="mantra-count">5</span>
                 </span>
             </a>
         </div>
-        <div class="mantra-badge">
-            <a class="mantra-menu-icon" href="./Html/cart.html">
-                <span class="mantra-icon">
-          <i class="fa fa-shopping-cart"></i>
-          <span class="mantra-count">1</span>
+        <div className="mantra-badge">
+            <a className="mantra-menu-icon" href="./Html/cart.html">
+                <span className="mantra-icon">
+          <i className="fa fa-shopping-cart"></i>
+          <span className="mantra-count">1</span>
                 </span>
 
-                <span class="mantra-cart-button">Cart</span>
+                <span className="mantra-cart-button">Cart</span>
             </a>
         </div>
     </div>
