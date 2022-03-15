7668fc940393554f7a6ae1659a2683bbfe2d85c4
Raghav Bang <raghav.bang@blueconchtech.com>
2022-03-15 18:45:12 +0530

    Added baner

--- src/pages/landing/Home.js
+++ src/pages/landing/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { CategoryBaner } from "../../components/categorybanner/CategoryBanner";
 import { Navbar } from "../../components/shared/navbar/Navbar";
 export function  Home(){
     return(
         <div>
         <Navbar/>
+        <CategoryBaner/>
         </div>
     )
 }
