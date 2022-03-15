e694f9337faa0c288c6bc543084f72ef4517b57c
Raghav Bang <raghav.bang@blueconchtech.com>
2022-03-15 19:15:53 +0530

    CSS added

--- src/pages/landing/Home.js
+++ src/pages/landing/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { CategoryBaner } from "../../components/categorybanner/CategoryBanner";
 import { Navbar } from "../../components/shared/navbar/Navbar";
+
+import "./home.css"
 export function  Home(){
     return(
         <div>
