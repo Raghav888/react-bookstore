31b58c22ab7cd7a51db9c9c4cf405e9cf8b710c4
Raghav Bang <raghav.bang@blueconchtech.com>
2022-03-16 10:42:19 +0530

    Added best seller & footer

--- src/components/shared/footer/Scrollfooter.js
+++ src/components/shared/footer/Scrollfooter.js
@@ -5,12 +5,12 @@ export const ScrollFooter = () => {
       <p>Created By Raghav Bang</p>
       <div class="mantra-footer-center">
         <a href="https://github.com/Raghav888 ">
-          <i class="fa fa-github mantra-footer-icon" aria-hidden="true "></i>
+          <i class="fa fa-github mantra-footer-icon" aria-hidden="true"></i>
         </a>
         <a href="https://www.linkedin.com/in/raghavbang/ ">
           <i
             class="fa fa-linkedin-square mantra-footer-icon"
-            aria-hidden="true "
+            aria-hidden="true"
           ></i>
         </a>
       </div>
