1357e47708ce535bb62bad6ecbd7f20d9ee1a40b
Raghav Bang <raghav.bang@blueconchtech.com>
2022-03-17 15:07:37 +0530

    Added productlist

--- src/pages/productListing/ProductListing.js
+++ src/pages/productListing/ProductListing.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Filterbox } from "../../components/filterbox/Filterbox";
+import { Products } from "../../components/products/Product";
+import { ScrollFooter } from "../../components/shared/footer/Scrollfooter";
 import { Navbar } from "../../components/shared/navbar/Navbar";
 
 export function ProductListing() {
   return (
     <div className="product-list-head">
       <Navbar />
-      <Filterbox />
+      <div className="main-box">
+        <Filterbox />
+        <Products />
+      </div>
+      <ScrollFooter />
     </div>
   );
 }
